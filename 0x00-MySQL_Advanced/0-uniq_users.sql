26168786829a060f1693b98a1c4b3943fe1dcfe8
Bbr011 <kahdraouih7@gmail.com>
2024-10-17 17:03:40 +0000

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,3 @@
+-- create the table unique_id on  MySQL server
+-- creates the table
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(256) NOT NULL UNIQUE, name VARCHAR(256));
